88617d71fb05502ecc5c36a27e05d4cdec6e91a8
laurianderson <laura.anderson@mercadolibre.com>
2023-05-09 18:31:58 -0300

    feat: simulator mock

---
+++ simulator/simulator_mock.go
@@ -0,0 +1,29 @@
+package simulator
+
+import "github.com/stretchr/testify/mock"
+
+type CatchSimulatorMockTestify struct {
+	//  provides a system by which it is possible to mock your objects and verify calls are happening as expected
+	// advantage of the library: it has a method you don't have to do the spy as in the manual way
+	mock.Mock
+}
+
+// the "Testify" mocking library is being used to create a CatchSimulator mock object (CatchSimulatorMockTestify) 
+//that implements the same interface as the actual CatchSimulator class
+func (cm *CatchSimulatorMockTestify) CanCatch(distance float64, speed float64, catchSpeed float64) bool {
+	args := cm.Called(distance, speed, catchSpeed)
+
+	// indicates that the return value of the first mock method call at position 0 of the registered return values is being retrieved
+	r0 := args.Bool(0)
+
+	return r0
+}
+
+func (cm *CatchSimulatorMockTestify) GetLinearDistance(position [2]float64) float64 {
+	args := cm.Called(position)
+
+	r0 := args.Get(0).(float64)
+
+	return r0
+}
+
