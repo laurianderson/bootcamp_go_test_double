ebbe2aeb1448a759f502b792d2fa10677a92adbb
laurianderson <laura.anderson@mercadolibre.com>
2023-05-10 13:07:59 -0300

    feat: add white shark test

--- prey/tuna.go
+++ prey/tuna.go
@@ -1,6 +1,9 @@
 package prey
 
-import "math/rand"
+import (
+	"math/rand"
+	"testdoubles/pkg/storage"
+)
 
 type tuna struct {
 	// max speed in m/s
@@ -11,7 +14,7 @@ func (t *tuna) GetSpeed() float64 {
 	return t.maxSpeed * rand.Float64()
 }
 
-func CreateTuna() Prey {
+func CreateTuna(s storage.Storage) Prey {
 	return &tuna{
 		maxSpeed:  252,
 	}
--- shark/white_shark_test.go
+++ shark/white_shark_test.go
@@ -1,13 +1,16 @@
 package shark
 
 import (
+	"fmt"
 	"testdoubles/pkg/storage"
 	"testdoubles/prey"
 	"testdoubles/simulator"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 	// "github.com/stretchr/testify/assert"
 )
-
+/*
 func TestWhiteShark_Hunt(t *testing.T) {
 	// arrange
 	storage := storage.NewStorageMock()
@@ -31,4 +34,27 @@ func TestWhiteShark_Hunt(t *testing.T) {
 	// expected called
 	//assert.True(t, sm.CanCatch())
  
+}
+*/
+func TestWhiteSharkHuntFailedSpeed(t *testing.T) {
+	// arrange
+	values := map[string]interface{}{
+		"white_shark_speed": 5.0,
+		"white_shark_x":     0.0,
+		"white_shark_y":     1.0,
+		"tuna_speed":        10.0,
+	}
+	storage := storage.NewMockStorage(values)
+	whiteSharkInstance := whiteShark{
+		speed:     5.0,
+		position:  [2]float64{1.0, 2.0},
+		simulator: simulator.NewCatchSimulator(2.0),
+	}
+	preyInstance := prey.CreateTuna(storage)
+
+	// act
+	result := whiteSharkInstance.Hunt(preyInstance)
+
+	// assert
+	assert.Equal(t, fmt.Errorf("could not hunt the prey"), result)
 }
