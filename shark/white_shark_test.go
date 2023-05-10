d9f252bcb5e4cdc4bb5489e4764bdd095d96dccf
laurianderson <laura.anderson@mercadolibre.com>
2023-05-10 13:10:34 -0300

    update: clean code test

--- shark/white_shark_test.go
+++ shark/white_shark_test.go
@@ -6,34 +6,11 @@ import (
 	"testdoubles/prey"
 	"testdoubles/simulator"
 	"testing"
-
 	"github.com/stretchr/testify/assert"
-	// "github.com/stretchr/testify/assert"
 )
 /*
 func TestWhiteShark_Hunt(t *testing.T) {
-	// arrange
-	storage := storage.NewStorageMock()
-	pr := prey.NewPreyStub()
-	pr.GetSpeedFn = func() float64 {
- 		return 0
- 	}
-	sm := simulator.NewCatchSimulatorMockTestify()
-	sm.On("CanCatch", 0.0, 0.0, 0.0).Return(true)
- 	sm.On("GetLinearDistance", [2]float64{1,2}).Return(0.0)
-
- 	sh := CreateWhiteShark(sm)
-
- 	// act
-	sh.Hunt(pr)
-
-	// assert
-	//sm.AssertCalled(t, "CanCatch", 0.0, 0.0, 0.0)
-	//sm.AssertCalled(t, "GetLinearDistance", [2]float64{1,2})
-	sm.AssertCalled(t, "Hunt")
-	// expected called
-	//assert.True(t, sm.CanCatch())
- 
+	MODIFY DO WITH THE FUNCTIONALITIES OF THE MOCK PACKAGE
 }
 */
 func TestWhiteSharkHuntFailedSpeed(t *testing.T) {
